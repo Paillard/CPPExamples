c6ca548471bf51cdae39e11608101887493d7c0b
Julien Treiber <julien.treiber@epitech.eu>
2016-11-21 12:52:16 +0100

    add few more manipulations on variables

--- src/variables.cpp
+++ src/variables.cpp
@@ -1,11 +1,24 @@
 // operations with variables
 #include <iostream>
+#include <string>
 
 int main()
 {
   // declaring variables
-  int a, b;
-  int result;
+  int a = 0;      // c-like initialization
+  int b (0);      // constructor initialization
+  int result {0}; // uniform initialization
+
+  int foo (0);
+  auto bar (foo); // compiler will determine bar's type
+  decltype(foo) baar;
+  decltype(foo) baar2 (5);
+
+  // simulate use
+  (void)bar;
+  (void)baar;
+  // actual use
+  std::cout << baar2 << std::endl;
 
   // process
   a = 5;
@@ -16,6 +29,10 @@ int main()
   // print the result
   std::cout << result << std::endl;
 
+  // using strings
+  std::string oneString ("So this is what a string looks like");
+
+  std::cout << oneString << std::endl;
   // terminate the program
   return 0;
 }
