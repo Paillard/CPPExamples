548dba1f28e30cb766700e3a59f7d077b6b7e5ac
Julien Treiber <julien.treiber@epitech.eu>
2016-11-21 12:33:04 +0100

    add proper ending for first program. Add second programm

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -3,5 +3,9 @@
 
 int	main()
 {
+	// print Hello World
 	std::cout << "Hello World!" << std::endl;
+
+	// terminate the program
+	return 0;
 }
---
+++ src/variables.cpp
@@ -0,0 +1,21 @@
+// operations with variables
+#include <iostream>
+
+int main()
+{
+  // declaring variables
+  int a, b;
+  int result;
+
+  // process
+  a = 5;
+  b = 2;
+  a = a + 1;
+  result = a - b;
+
+  // print the result
+  std::cout << result << std::endl;
+
+  // terminate the program
+  return 0;
+}
