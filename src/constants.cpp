0bc0781449c7be631036c54d8c8ddd965781f9eb
Julien Treiber <julien.treiber@epitech.eu>
2016-11-21 13:01:13 +0100

    start constants examples

---
+++ src/constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  // they're all the same.
+  std::cout << 75 << std::endl;   // decimal
+  std::cout << 0113 << std::endl; // octal
+  std::cout << 0x4b << std::endl; // hexadecimal
+
+  return 0;
+}
